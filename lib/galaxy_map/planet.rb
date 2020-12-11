a74599e6954c619bc1e25432e88a6bad637ac407
Hunter <fleakhunter@gmail.com>
2020-12-11 14:59:22 -0500

    Added attribute accessors for the information specified for each planet.

--- lib/galaxy_map/planet.rb
+++ lib/galaxy_map/planet.rb
@@ -1,5 +1,6 @@
 class Planet
     @@all = []
+    attr_accessor :name, :rotation_period, :orbital_period, :diameter, :climate, :gravity, :terrain, :surface_water, :population, :residents, :films
 
     def save
         self.class.all << self
@@ -7,5 +8,11 @@ class Planet
 
     def self.all
         @@all
+    end
+
+    if attributes
+        attributes.each do |k,v|
+          self.send("#{k}=", v)
+        end
     end
 end
