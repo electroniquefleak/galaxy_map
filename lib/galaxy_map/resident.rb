9c231a02ad4e94105b9029bf36f5a993d61e3826
Hunter <fleakhunter@gmail.com>
2020-12-12 14:28:19 -0500

    Copied over syntax from the Planet class, applied appropriate attributes for the Resident class to be initialized in galaxy_map.rb.

--- lib/galaxy_map/resident.rb
+++ lib/galaxy_map/resident.rb
@@ -1,12 +1,31 @@
 class Resident
     @@all = []
     attr_accessor :name, :height, :mass, :hair_color, :skin_color, :eye_color, :birth_year, :gender, :homeworld, :films, :species, :vehicles, :starships, :created, :edited, :url
-    
+
     def save
         self.class.all << self
     end
 
     def self.all
         @@all
+    end
+
+    def initialize(attributes=nil)
+        if attributes
+            attributes.each do |k,v|
+            #puts k
+            #puts v
+              self.send("#{k}=", v)
+            end
+        end
+        save
+    end
+
+    def to_s
+        "#{@name}"
+    end
+
+    def info
+        "#{name}" + "\n" + "#{height}" + "\n" + "#{mass}" + "\n" + "#{hair_color}" + "\n" + "#{eye_color}" + "\n" + "#{birth_year}" + "\n" + "#{gender}" + "\n" + "#{homeworld}" + "\n" + "#{films}" + "\n" + "#{species}"+ "\n" + "#{vehicles}" + "\n" + "#{starships}"
     end
 end
