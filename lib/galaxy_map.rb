f088ea4184438b140c8287d110b5b18a3cb79909
Hunter <fleakhunter@gmail.com>
2020-12-12 16:11:03 -0500

    Removed the Film class and file, application did not require that level of information. Moved the methods in galaxy_map.rb to cli.rb as a method (welcome).

--- lib/galaxy_map.rb
+++ lib/galaxy_map.rb
@@ -7,7 +7,6 @@ require "json"
 require_relative "./galaxy_map/version"
 require_relative "./galaxy_map/api"
 require_relative "./galaxy_map/cli"
-require_relative "./galaxy_map/film"
 require_relative "./galaxy_map/planet"
 require_relative "./galaxy_map/resident"
 
@@ -16,30 +15,3 @@ module GalaxyMap
   # Your code goes here...
 end
 
-api=API.new
-(1..6).each do |i|
-planets=api.get_planets(i)
-planets.each do |planet|
-Planet.new(api.get_planet(planet))
-end
-end
-puts "Welcome to the Jedi Archives, student. There is more knowledge here than anywhere else in the galaxy!"
-Planet.all.each_with_index do |planet, i|
-  puts "#{i}: #{planet}"
-end
-print "Total Planets:" + " "
-puts Planet.all.count
-while true
-puts "Galaxy Map Interface: Intialized. . . Welcome, please select a planet to view its astrographical information."
-choice = gets.chomp.to_i
-puts Planet.all[choice].info
-puts "Would you like to select another planet? (Y/N)"
-answer = gets.chomp
-if answer.downcase == "no"
-  break
-else 
-  Planet.all.each_with_index do |planet, i|
-  puts "#{i}: #{planet}"
-end
-end
-end
--- lib/galaxy_map/cli.rb
+++ lib/galaxy_map/cli.rb
@@ -5,6 +5,34 @@ class CLI
     end
 
     def welcome
-        #puts "Welcome to the galaxy map. There is more knowledge here than anywhere else in the galaxy!"
+        api=API.new
+        (1..6).each do |i|
+            planets=api.get_planets(i)
+            planets.each do |planet|
+                Planet.new(api.get_planet(planet))
+            end
+        end
+    
+        puts "Welcome to the Jedi Archives, student. There is more knowledge here than anywhere else in the galaxy!"
+        Planet.all.each_with_index do |planet, i|
+          puts "#{i}: #{planet}"
+        end
+
+        print "Total Planets:" + " "
+        puts Planet.all.count
+        while true
+            puts "Galaxy Map Interface: Intialized. . . Welcome, please select a planet to view its astrographical information."
+            choice = gets.chomp.to_i
+            puts Planet.all[choice].info
+            puts "Would you like to select another planet? (Y/N)"
+            answer = gets.chomp
+            if answer.downcase == "no"
+                break
+            else 
+            Planet.all.each_with_index do |planet, i|
+                puts "#{i}: #{planet}"
+                end
+            end
+        end
     end
 end
